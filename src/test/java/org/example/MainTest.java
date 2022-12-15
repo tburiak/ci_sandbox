2aca3231b1f966e7c7dde7f7fe832519aaa41014
Taras Buriak <tarasburyak@gmail.com>
2022-12-15 18:13:35 +0200

    Initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(true);
+    }
+
+}
